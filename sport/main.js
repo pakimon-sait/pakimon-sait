32c0aaaa69e882acec0478eee3121ae0f9c04c58
pakimon-sait <129604781+pakimon-sait@users.noreply.github.com>
2024-05-28 18:58:19 +0300

    sport

---
+++ sport/main.js
@@ -0,0 +1,383 @@
+
+//--------------------------------------------------//
+
+let btnTop = document.querySelector('.move-top');
+btnTop.style.display = 'none'
+function showBtnTop() {
+
+    let height = window.innerHeight;
+    console.log(height + 'height');
+    if (window.scrollY > height) {
+        btnTop.style.display = 'flex'
+    }
+    else {
+        btnTop.style.display = 'none'
+    }
+}
+window.addEventListener('scroll', showBtnTop);
+
+//----------------------------------------//
+
+ScrollReveal().reveal('.top-1', {
+    origin: 'top',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1000,
+});
+
+ScrollReveal().reveal('.top-2', {
+    origin: 'top',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1200,
+});
+ScrollReveal().reveal('.top-3', {
+    origin: 'top',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1400,
+});
+ScrollReveal().reveal('.top-4', {
+    origin: 'top',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1600,
+});
+ScrollReveal().reveal('.top-5', {
+    origin: 'top',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1800,
+});
+// ----------------------------------------------//
+
+ScrollReveal().reveal('.left-1', {
+    origin: 'left',
+    distance: '500px',
+    reset: 'true',
+    delay: 100,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-2', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 500,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-3', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 600,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-4', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 700,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-5', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 800,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-6', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 900,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-7', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1000,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-8', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1100,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-9', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1200,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-10', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1300,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-11', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1400,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-12', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1500,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-13', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1600,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-14', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1700,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-15', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1800,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-16', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1900,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-17', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 2000,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-18', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 2100,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-19', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 2200,
+    duration: 2000,
+});
+ScrollReveal().reveal('.left-20', {
+    origin: 'left',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 2300,
+    duration: 2000,
+});
+//------------------------------------------------------------//
+
+ScrollReveal().reveal('.right-1', {
+    origin: 'rigth',
+    distance: '500px',
+    reset: 'true',
+    delay: 500,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-2', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 500,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-3', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 600,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-4', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 700,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-5', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 800,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-6', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 900,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-7', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1000,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-8', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1100,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-9', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1200,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-10', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1300,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-11', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1400,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-12', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1500,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-13', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1600,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-14', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1700,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-15', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1800,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-16', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 1900,
+    duration: 2000,
+});
+ScrollReveal().reveal('.right-17', {
+    origin: 'right',
+    distance: '500px',
+    interval: 500,
+    reset: 'true',
+    delay: 2000,
+    duration: 2000,
+});
+//----------------------------------------------------------//
+
+ScrollReveal().reveal('.down-1', {
+    origin: 'bottom',
+    distance: '50px',
+    interval: 200,
+    reset: 'true',
+    delay: 800,
+    duration: 1000,
+});
+
+ScrollReveal().reveal('.down-2', {
+    origin: 'bottom',
+    distance: '50px',
+    interval: 200,
+    reset: 'true',
+    delay: 800,
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.down-3', {
+    origin: 'bottom',
+    distance: '50px',
+    interval: 200,
+    reset: 'true',
+    delay: 800,
+    duration: 1400,
+});
+
